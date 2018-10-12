708f178cd2fe22a1656b22e6dc7ee0ffd4ef6a00
YINJIE WU <2721541713@qq.com>
2018-10-12 15:12:10 -0400

    Basic Duckman Sprites loaded

--- js/game0.js
+++ js/game0.js
@@ -3,10 +3,14 @@ let game0State = function(){
 };
 
 game0State.prototype.create = function(){
-	this.BG = game.add.sprite(0, 0, "DuckBG");
+	game.add.sprite(0, 0, "BlueW");
+	this.BG = game.add.sprite(155.5, 137.5, "DuckBG");
 	this.BG.animations.add("only", [0, 1], 5, true);
 	this.BG.animations.play("only");
 
+	this.player = game.add.sprite(155.5, 137.5, "Duck");
+	this.player.animations.add("only", [0, 1], 5, true);
+	this.player.animations.play("only");
 	
 };
 
