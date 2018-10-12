ab85bc2f602ba969559e3895359bd6d09e63a537
YINJIE WU <2721541713@qq.com>
2018-10-12 15:00:50 -0400

    fixed Duckman Background

--- js/preload.js
+++ js/preload.js
@@ -8,7 +8,7 @@ preloadState.prototype.preload = function(){
 	game.load.image("sky", "assets/sky.png");
 	game.load.image("platform", "assets/platform.png");
 	game.load.image("star", "assets/star.png");
-	game.load.spritesheet("DuckBG", "assets/BGSpriteSheetDuckman.png", 2125, 850);
+	game.load.spritesheet("DuckBG", "assets/BGSpriteSheet2.png", 2125, 850);
 	game.load.spritesheet("button", "assets/button_sprite_sheet_StartAgainExit.png", 192, 71);
 	game.load.spritesheet("murph", "assets/character.png", 32, 48);
 };
