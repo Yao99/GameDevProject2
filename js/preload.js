aad65ae8eef702b234577de4b5f63554bb54f700
YINJIE WU <2721541713@qq.com>
2018-10-15 16:20:14 -0400

    Transition animation added

--- js/preload.js
+++ js/preload.js
@@ -16,7 +16,18 @@ preloadState.prototype.preload = function(){
 	game.load.spritesheet("Duck", "assets/duckmanCar_sheetpixeld.png", 300, 301);
 	game.load.spritesheet("BlueTed", "assets/DMBlueTed_sheet.png", 100, 200);
 	game.load.spritesheet("PinkTed", "assets/DMPinkTedsheet.png", 100, 200);
-	game.load.image("Transition", "assets/TransSpriteSheet.png");
+	game.load.image("Transition1", "assets/Transition PNGs/Ta1.png");
+	game.load.image("Transition2", "assets/Transition PNGs/Ta2.png");
+	game.load.image("Transition3", "assets/Transition PNGs/Ta3.png");
+	game.load.image("Transition4", "assets/Transition PNGs/Ta4.png");
+	game.load.image("Transition5", "assets/Transition PNGs/Ta5.png");
+	game.load.image("Transition6", "assets/Transition PNGs/Ta6.png");
+	game.load.image("Transition7", "assets/Transition PNGs/Ta7.png");
+	game.load.image("Transition8", "assets/Transition PNGs/Ta8.png");
+	game.load.image("Transition9", "assets/Transition PNGs/Ta9.png");
+	game.load.image("Transition10", "assets/Transition PNGs/Ta10.png");
+	game.load.image("Transition11", "assets/Transition PNGs/Ta11.png");
+	game.load.image("Transition12", "assets/Transition PNGs/Ta12.png");
 	
 	//whale game
 	game.load.tilemap("WhaleMap", "assets/whale.json", null, Phaser.Tilemap.TILED_JSON);
--- js/trans.js
+++ js/trans.js
@@ -1,15 +1,53 @@
 let transState = function(){
-
+	counter = 0;
 };
 
 transState.prototype.create = function(){
 	this.WBG = game.add.sprite(0, 0, "BlueW");
-	this.BG = game.add.image(155.5, 137.5, "Transition");
+	this.BG = game.add.image(155.5, 137.5, "Transition1");
 	
 	
 };
 
 transState.prototype.update = function(){
+	counter++;
+	if(counter == 100){
+		this.BG = game.add.image(155.5, 137.5, "Transition2")
+	}
+	if(counter == 140){
+		this.BG = game.add.image(155.5, 137.5, "Transition3")
+	}
+	if(counter == 170){
+		this.BG = game.add.image(155.5, 137.5, "Transition4")
+	}
+	if(counter == 190){
+		this.BG = game.add.image(155.5, 137.5, "Transition5")
+	}
+	if(counter == 210){
+		this.BG = game.add.image(155.5, 137.5, "Transition6")
+	}
+	if(counter == 230){
+		this.BG = game.add.image(155.5, 137.5, "Transition7")
+	}
+	if(counter == 250){
+		this.BG = game.add.image(155.5, 137.5, "Transition8")
+	}
+	if(counter == 270){
+		this.BG = game.add.image(155.5, 137.5, "Transition9")
+	}
+	if(counter == 290){
+		this.BG = game.add.image(155.5, 137.5, "Transition10")
+	}
+	if(counter == 310){
+		this.BG = game.add.image(155.5, 137.5, "Transition11")
+	}
+	if(counter == 330){
+		this.BG = game.add.image(155.5, 137.5, "Transition12")
+	}
+	if(counter == 350){
+		this.BG = game.add.image(155.5, 137.5, "Transition11")
+	}
+
 };
 
 function actionOnClick () {
